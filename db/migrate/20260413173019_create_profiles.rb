282ada667465b846f3f32bf7dc68e7cf449e9938
Lautaro Gartner <158626277+LautaroGartner@users.noreply.github.com>
2026-04-13 14:46:43 -0300

    db migrate createprofiles class fixed

--- db/migrate/20260413173019_create_profiles.rb
+++ db/migrate/20260413173019_create_profiles.rb
@@ -1,10 +1,10 @@
 class CreateProfiles < ActiveRecord::Migration[8.1]
   def change
     create_table :profiles do |t|
-      t.references :user, null: false, foreign_key: true
-      t.string :display_name
-      t.text :bio
-      t.string :location
+      t.references :user, null: false, foreign_key: { on_delete: :cascade }, index: { unique: true }
+      t.string :display_name, limit: 80
+      t.text   :bio,          limit: 500
+      t.string :location,     limit: 80
 
       t.timestamps
     end
