e405294b38738eef687be9d2339145d85fa5938a
Lautaro Gartner <158626277+LautaroGartner@users.noreply.github.com>
2026-08-12 17:18:41 -0300

    Apply branded sender through Action Mailer defaults

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -24,8 +24,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  mailer_address = ENV.fetch("MAILER_FROM", "contact@paideiastudios.com")
-  config.mailer_sender = mailer_address.include?("<") ? mailer_address : "Lubook <#{mailer_address}>"
+  config.mailer_sender = ENV.fetch("MAILER_FROM", "contact@paideiastudios.com")
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
